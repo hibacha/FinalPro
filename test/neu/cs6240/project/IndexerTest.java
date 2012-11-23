071b7051c1c9abec35e8768073f1d76150a38062
bacha <zhouyaofei@gmail.com>
2012-11-22 21:53:29 -0500

    test

---
+++ test/neu/cs6240/project/IndexerTest.java
@@ -0,0 +1,24 @@
+package neu.cs6240.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IndexerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
