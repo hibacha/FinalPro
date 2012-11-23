7e2dd6fdfc8ae7b15ab2628d1d39917dd640f71c
bacha <zhouyaofei@gmail.com>
2012-11-22 21:33:29 -0500

    generate folder

---
+++ src/neu/cs6240/project/Indexer.java
@@ -0,0 +1,13 @@
+package neu.cs6240.project;
+
+public class Indexer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("team");
+	}
+
+}
